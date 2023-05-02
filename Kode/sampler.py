88adeeb9e1918797bf1a2f1be1ce6326b61c8bf8
Viveka Priya Simhan <vivekaps@stud.ntnu.no>
2023-05-02 17:10:11 +0200

    verbosity in sampler

--- Kode/sampler.py
+++ Kode/sampler.py
@@ -15,16 +15,27 @@ class Sampler:
 
     def posterior(self) -> dict:
         raise NotImplementedError
+    
+    def verbosity(self, verbose: bool) -> function:
+        if verbose:
+            def verboseprint(*args):
+                for arg in args:
+                    print(arg)
+                return
+        else:
+            verboseprint = lambda *args: None
+        return verboseprint
 
 class RejectionSampler(Sampler):
     def __init__(self, y: np.array, prior: Model, m: Model, s: Statistic, distance: Distance) -> None:
         super().__init__(y, prior, m, s, distance)
 
-    def posterior(self, Nsim = 10**6, quant = 0.001, splits = None) -> dict:
+    def posterior(self, Nsim = 10**6, quant = 0.001, splits = None, verbose = False) -> dict:
         """
         returns approximate posterior + information
         given quantile of n samples are returned
         """
+        verboseprint = self.verbosity(verbose)
         if splits is not None:
             Nsim = abs( -Nsim // splits)
         else:
@@ -34,6 +45,7 @@ class RejectionSampler(Sampler):
         s0 = self.s.statistic(self.obs)
         post = np.zeros((d, 0))
         for i in range(splits):
+            verboseprint('Starting split {} out of {}'.format(i + 1, splits))
             proposals = self.prior.simulate(Nsim = Nsim)
             z = self.model.simulate(parameters = proposals, Nsim = Nsim)
             sz = self.s.statistic(z) 
@@ -41,6 +53,7 @@ class RejectionSampler(Sampler):
             qval = np.quantile(d, q = quant)
             id = np.where(d < qval)[0]
             post = np.concatenate((post, proposals[:, id]), axis = 1)
+            verboseprint('In split {} the tolerance was {}.'.format(i + 1, qval))
         return {'distribution' : post, 'tolerance' : qval}
 
 
